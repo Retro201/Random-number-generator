5aa6e8fb8aa4c8b4c2d64abf5a3caf40385a8219
Retro201 <b8zsyhkdbm@privaterelay.appleid.com>
2026-05-17 20:47:15 -0500

    Update game.lua

--- game.lua
+++ game.lua
@@ -3,6 +3,9 @@ Math.randomseed(os.time())
 function clearscreen()
     os.execute("clear")
 end
+function sleep(n)
+    os.execute("timeout /T" .. tonumber(n))
+end
 
 local low = 1
 
@@ -18,7 +21,9 @@ function startGame()
         local number = Math.random(low, high)
         local typed = os.read()
         if typed == number then
+
             print("The number was correct. Returning...")
+            sleep()
         end
     end
 end
